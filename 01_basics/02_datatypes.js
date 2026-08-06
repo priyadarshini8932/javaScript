43e81eda372b04ed92c4f766380426c745ee1cbe
Shreya Priyadarshini <shreyapriyadarshini032@gmail.com>
2026-08-06 07:07:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using node.js, not browser
+
+console.log(3
+    +
+     3) //code readability should be high
+console.log("Shreya")
+
+let name="Shreya"
+let age=23
+let isLoggedIn=true
+
+//PREMITIVE DATA TYPE
+//number =>2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null (standalone value) =>jb khali hai
+//undefined => jb value define nhi ki
+//symbol =>unique
+
+//NON PRIMITIVE DATA TYPE
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
