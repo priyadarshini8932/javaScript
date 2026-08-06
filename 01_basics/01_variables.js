da579ac68a7025a7c44ecaa5aceb2640c2d474d6
Shreya Priyadarshini <shreyapriyadarshini032@gmail.com>
2026-08-06 07:06:57 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="shreya@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2 //not allowed
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Patna"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+console.log(accountState)
+//use const and let not var
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
